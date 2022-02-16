cf21f5c081805ff613b03d98233fab42a7957d11
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2022-02-16 15:43:19 +0500

    seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $path = base_path() . '/database/data.sql';
+        $sql = file_get_contents($path);
+        DB::unprepared($sql);
     }
 }
